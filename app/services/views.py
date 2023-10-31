ca60829f39aeb17d7b210436655a7cdbaaaed477
Oleg Remizov <job.rem1zoff@gmail.com>
2023-10-31 17:20:43 +0300

    optimized Subscription query

--- app/services/views.py
+++ app/services/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from .models import Subscripton
+from .models import Subscripton, Client
 from .serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscripton.objects.all()
+    # queryset = Subscripton.objects.all().prefetch_related('client').prefetch_related('client__user')
+    queryset = Subscripton.objects.all().prefetch_related(
+        Prefetch('client', # присоединит клиентов where id in [1, 2....]
+    queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')) # только нужные поля
+    )
     serializer_class = SubscriptionSerializer
