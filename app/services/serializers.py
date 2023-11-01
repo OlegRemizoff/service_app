332c4ec5bbf1c13241945ac0f80c287caf9f8fe4
Oleg Remizov <job.rem1zoff@gmail.com>
2023-11-01 13:00:19 +0300

    added inserted serializer plan in Subscr serializer and optimization

--- app/services/serializers.py
+++ app/services/serializers.py
@@ -1,14 +1,26 @@
 from rest_framework import serializers
-from .models import Subscripton
+from .models import Subscripton, Plan
 
 
+
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Plan
+        fields = '__all__'
 
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
 
     class Meta:
         model = Subscripton
-        fields = ('id', 'plan_id', 'client_name', 'email') # plan_id - т.к ForeignKey, ссылается на Plan.id
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan') # plan_id - т.к ForeignKey, ссылается на Plan.id
+
+
+
--- app/services/views.py
+++ app/services/views.py
@@ -7,7 +7,14 @@ from .serializers import SubscriptionSerializer
 class SubscriptionView(ReadOnlyModelViewSet):
     # queryset = Subscripton.objects.all().prefetch_related('client').prefetch_related('client__user')
     queryset = Subscripton.objects.all().prefetch_related(
+        'plan', # оптимизирует проблему n+1 - FROM "services_plan" WHERE "services_plan"."id" IN (2, 3)
         Prefetch('client', # присоединит клиентов where id in [1, 2....]
     queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')) # только нужные поля
     )
     serializer_class = SubscriptionSerializer
+
+''' n+1 при добавлении вложенного серализаторо plan
+(0.000) SELECT "services_plan"."id", "services_plan"."plan_types", "services_plan"."discount_percent" FROM "services_plan" WHERE "services_plan"."id" = 3 LIMIT 21; args=(3,); alias=default
+(0.000) SELECT "services_plan"."id", "services_plan"."plan_types", "services_plan"."discount_percent" FROM "services_plan" WHERE "services_plan"."id" = 2 LIMIT 21; args=(2,); alias=default
+(0.000) SELECT "services_plan"."id", "services_plan"."plan_types", "services_plan"."discount_percent" FROM "services_plan" WHERE "services_plan"."id" = 3 LIMIT 21; args=(3,); alias=default
+'''
