970e940903a24e9ab262a4e03ed430ffdadce945
Oleg Remizov <job.rem1zoff@gmail.com>
2023-11-02 17:14:43 +0300

    created method get_price

--- app/services/serializers.py
+++ app/services/serializers.py
@@ -17,10 +17,16 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
+    price = serializers.SerializerMethodField() # ищет функцию get_имя поля
+
+    def get_price(self, instance): # instance = Subcriptions
+        return (instance.service.full_price - 
+               (instance.service.full_price * instance.plan.discount_percent / 100))
+
 
     class Meta:
         model = Subscripton
-        fields = ('id', 'plan_id', 'client_name', 'email', 'plan') # plan_id - т.к ForeignKey, ссылается на Plan.id
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')# plan_id-т.к ForeignKey, ссылается на Plan.id
 
 
 
